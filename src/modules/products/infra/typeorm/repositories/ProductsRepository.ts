5e55468b2b085ec90225605776700f706d182a60
Wladimir <wladimir.code@gmail.com>
2020-09-13 21:02:00 -0300

    feat: Products implmentation, Service, Repository, controller

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,35 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const findProduct = await this.ormRepository.findOne({
+      where: { name },
+    });
+
+    return findProduct;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const findProductsById = await this.ormRepository.findByIds(products);
+
+    return findProductsById;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const findProductsById = await this.ormRepository.findByIds(products);
+
+    Object.assign(findProductsById, products);
+
+    return findProductsById;
   }
 }
 
